e49a453a84ec2219a5363faa2bf9120ef6f6aac0
JenifferGenoves <62884668+JenifferGenoves@users.noreply.github.com>
2025-11-05 20:53:07 +0000

    Changes with button

--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -1,13 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const TrafficLight = () => {
     // Componente global para manejar estado
     const [selected, setSelected] = useState("red-light");
 
+    // Crear componente para que arranque en automático
+    const [active, setActive] = useState(false);
+
     const changeColor = (color) => {
         setSelected(color)
     }
 
+// Cada 3 seg la luz se cambia automáticamente
+
+    // useEffect (() => {}, []) Si no se pone las dependencias [], se ejecuta siempre
+
+    useEffect (() => {
+        if(active) {
+            const interval = setInterval(() => {
+                if(selected == "red-light") {
+                    setSelected("yellow-light");
+                } else if (selected == "yellow-light") {
+                    setSelected("green-light");
+                } else if (selected == "green-light") {
+                    setSelected("red-light");
+                };
+
+            }, 1000)
+            
+            return () => clearInterval(interval)
+        };
+
+    }, [active, selected])
 
     return (
         <main>
@@ -29,6 +53,9 @@ const TrafficLight = () => {
                 onClick={() => changeColor("green-light")}
                 ></div>
                     
+            </div>
+            <div className="btn-light">
+                <button onClick={() =>setActive(!active)} className="btn btn-secondary button">Change Lights</button>
             </div>
         </main>
     )
